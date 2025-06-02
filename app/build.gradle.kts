dd4419f1abb3f966d74e850a6d11ba8f4c5adccc
Fir3n0x <corentinmahieu50@gmail.com>
2025-06-02 22:30:39 +0200

    Correction black screen launch application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "2.11.0"
+        versionName = "2.11.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
