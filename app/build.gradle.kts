687bce0ea929013d638c343050c4332a3bc38aa4
Fir3n0x <corentinmahieu50@gmail.com>
2026-02-14 17:03:24 +0100

    Add keystore signing to GitHub Actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "2.21.6"
+        versionName = "2.21.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
